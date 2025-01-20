79bf101263de0c8f4a350cf72ce8f59db6c8ecfc
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2025-01-21 01:45:23 +0200

    return exercise: 11.12 Health check

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  if (true) throw 'error...  ' // eslint-disable-line no-constant-condition
   res.send('ok')
 })
 
